72500897dff130b15e2e1758fee6c461a7de9628
RohitKanderi <97522995+RohitKanderi@users.noreply.github.com>
2022-08-16 11:16:51 +0530

    Add files via upload

---
+++ develop/hello.java
@@ -0,0 +1,14 @@
+package com.coditas.file_handling;
+
+import java.util.List;
+
+public class hello {
+    public static void main(String[] args) {
+                System.out.println("Rohit");
+
+		        System.out.println("Rohit");
+        System.out.println("Rohit");
+
+        
+    }
+}
