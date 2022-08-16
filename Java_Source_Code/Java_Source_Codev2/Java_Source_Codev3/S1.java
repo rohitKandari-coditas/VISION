1484cc9b0023c7537756991fa88424c35f309170
RohitKanderi <97522995+RohitKanderi@users.noreply.github.com>
2022-08-16 11:15:28 +0530

    Add files via upload

---
+++ Java_Source_Code/Java_Source_Codev2/Java_Source_Codev3/S1.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ Java_Source_Code/Java_Source_Codev2/S3.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args){
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+        System.out.println("ArrayList: " + animals);
+    }
+}
